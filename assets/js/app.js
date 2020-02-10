4aa7c7d60de7c58aeb7dc1f8f326a1a3aad3f12a
test cloud <test.cloudweblabs@gmail.com>
2020-02-10 15:38:36 +0530

    Animation added now

---
+++ assets/js/app.js
@@ -0,0 +1,58 @@
+const uiElements = (function() {
+  const el = [
+    {
+      selector: "#js-features-animate",
+      offset: "70%",
+      effect: "animated slideInDown"
+    },
+    {
+      selector: "#js-work-animate",
+      offset: "70%",
+      effect: "animated fadeInRightBig"
+    },
+    {
+      selector: "#js-team-animate",
+      offset: "80%",
+      effect: "animated fadeInLeftBig"
+    },
+    {
+      selector: ".facts__item__symbol_container",
+      offset: "85%",
+      effect: "animated zoomInUp"
+    }
+  ];
+  return {
+    allUi: el
+  };
+})();
+
+const uiController = (function() {
+  const setUpwayPoint = item => {
+    const { selector, offset, effect } = item;
+    $(selector).waypoint(
+      function(direction) {
+        if (direction == "down") {
+          $(this.element).addClass(effect);
+        }
+      },
+      {
+        offset: offset
+      }
+    );
+  };
+  return {
+    setPoint: setUpwayPoint
+  };
+})();
+
+const appController = (function(ui, controller) {
+  const allUi = ui.allUi;
+  const setInit = () => {
+    allUi.forEach((item, index) => controller.setPoint(item));
+  };
+  return {
+    init: setInit
+  };
+})(uiElements, uiController);
+
+appController.init();
